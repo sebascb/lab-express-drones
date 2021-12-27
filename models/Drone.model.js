35fda708052a706a8440b5901f9a31df5b3dcbcb
Sebas Corral <sebascorralbrito@gmail.com>
2021-12-27 17:27:08 +0100

    model modified

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,12 @@
 // Iteration #1
+const mongoose = require("mongoose");
+
+const DroneSchema = new mongoose.Schema({
+    name : {type: String},
+    propellers: {type: Number},
+    maxSpeed: {type: Number}
+});
+
+const Drone = mongoose.model("Drone", DroneSchema);
+
+module.exports = Drone;
