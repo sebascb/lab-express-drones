136d3e9a4b37a0fcfd974e697fa03bf4d2392961
Sebas Corral <sebascorralbrito@gmail.com>
2021-12-27 17:15:32 +0100

    db index modified

--- db/index.js
+++ db/index.js
@@ -8,7 +8,12 @@ const mongoose = require("mongoose");
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
 
 mongoose
-  .connect(MONGO_URI)
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
   .then((x) => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
   })
